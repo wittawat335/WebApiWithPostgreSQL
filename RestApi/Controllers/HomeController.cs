bd4d7951180c72bf06c8fadf638f8d37513d51c0
Wittawat Fuangfu <wittawat335@gmail.com>
2023-08-03 16:26:44 +0700

    #

--- RestApi/Controllers/HomeController.cs
+++ RestApi/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace RestApi.Controllers
-{
+{//
     [Route("api/[controller]")]
     [ApiController]
     public class HomeController : ControllerBase
